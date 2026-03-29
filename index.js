c390dcd83411784068a6432a1488be1c02405825
xmouaouia <xmouaouia@gmail.com>
2026-03-29 15:20:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const Days=document.getElementById('days');
+const Hours=document.getElementById('hours');
+const Minutes=document.getElementById('minutes');
+const Seconds=document.getElementById('seconds');
+
+const targetDate = new Date("April 1 2026 00:00:00").getTime();
+
+function timer (){
+    const currentDate = new Date().getTime();
+    const distance = targetDate - currentDate;
+
+    const days = Math.floor(distance / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(distance / 1000 / 60 / 60)%24;
+    const minutes = Math.floor(distance / 1000 / 60)%60;
+    const seconds = Math.floor(distance / 1000)%60;
+
+   
+    Days.innerHTML = days;
+    Hours.innerHTML = hours;
+    Minutes.innerHTML = minutes;
+    Seconds.innerHTML = seconds;
+
+    if(distance <0){
+        Days.innerHTML = "0";
+        Hours.innerHTML = "0";
+        Minutes.innerHTML = "0";
+        Seconds.innerHTML = "0";
+    }
+}
+setInterval(timer, 1000);
+
+
+
